6ccd9d2c66fa98d1ba918f0b784cc170443f6e6d
khoa <dknguyen176@gmail.com>
2022-08-27 13:23:13 +0700

    update db

--- app/src/main/java/com/example/senocare/helper/SenoDB.java
+++ app/src/main/java/com/example/senocare/helper/SenoDB.java
@@ -9,11 +9,14 @@ import com.example.senocare.model.Patient;
 import com.example.senocare.model.Prescription;
 import com.example.senocare.model.Schedule;
 
+import org.bson.types.ObjectId;
+
 import java.util.concurrent.atomic.AtomicReference;
 
 import io.realm.Realm;
 import io.realm.RealmQuery;
 import io.realm.RealmResults;
+import io.realm.Sort;
 import io.realm.mongodb.App;
 import io.realm.mongodb.AppConfiguration;
 import io.realm.mongodb.Credentials;
@@ -26,7 +29,7 @@ import io.realm.mongodb.sync.SyncConfiguration;
 public final class SenoDB {
     private final static String appID = "senocare-uzrlz";
 
-    public static Realm realm = null;
+    private static Realm realm = null;
     public static App app;
     public static User user;
 
@@ -127,6 +130,15 @@ public final class SenoDB {
         realm = Realm.getInstance(config);
     }
 
+    public static void setUserType() {
+        Patient patient = realm.where(Patient.class).findFirst();
+        IS_PATIENT = (patient != null && patient.get_id().equals(user.getId()));
+    }
+
+    public static void setUserType(boolean bool) {
+        IS_PATIENT = bool;
+    }
+
     public static void insertPatient(Patient patient) {
         realm.executeTransaction(r -> {
             Patient newPatient = r.createObject(Patient.class, user.getId());
@@ -141,13 +153,46 @@ public final class SenoDB {
         });
     }
 
-    public static void setUserType() {
-        Patient patient = realm.where(Patient.class).findFirst();
-        IS_PATIENT = (patient != null && patient.get_id().equals(user.getId()));
+    public static void insertPrescription(Prescription prescription) {
+        realm.executeTransaction(r -> {
+            Prescription newPrescription = r.createObject(Prescription.class, new ObjectId());
+            newPrescription.set(prescription);
+        });
     }
 
-    public static void setUserType(boolean bool) {
-        IS_PATIENT = bool;
+    public static void insertMessage(Message message) {
+        realm.executeTransaction(r -> {
+            Message newMessage = r.createObject(Message.class, new ObjectId());
+            newMessage.set(message);
+        });
+    }
+
+    public static void insertSchedule(Schedule schedule) {
+        realm.executeTransaction(r -> {
+            Schedule newSchedule = r.createObject(Schedule.class, new ObjectId());
+            newSchedule.set(schedule);
+        });
+    }
+
+    public static void modifyPatient(Patient p) {
+        realm.executeTransaction(r -> {
+            Patient patient = r.where(Patient.class).equalTo("_id", p.get_id()).findFirst();
+            patient.set(p);
+        });
+    }
+
+    public static void modifyDoctor(Doctor d) {
+        realm.executeTransaction(r -> {
+            Doctor doctor = r.where(Doctor.class).equalTo("_id", d.get_id()).findFirst();
+            doctor.set(d);
+        });
+    }
+
+    public static void modifySchedule(ObjectId _id, String status) {
+        realm.executeTransaction(r -> {
+            Schedule schedule = r.where(Schedule.class).equalTo("_id", _id).findFirst();
+            schedule.setStatus(status);
+        });
     }
 
     public static RealmResults<Patient> getPatientList() {
@@ -155,11 +200,57 @@ public final class SenoDB {
     }
 
     public static RealmResults<Doctor> getDoctorList() {
-        return realm.where(Doctor.class).findAll();
+        return realm.where(Doctor.class)
+                .sort("rating", Sort.DESCENDING)
+                .findAll();
+    }
+
+    public static RealmResults<Doctor> getDoctorList(String spec) {
+        return realm.where(Doctor.class)
+                .equalTo("spec", spec)
+                .sort("rating", Sort.DESCENDING)
+                .findAll();
+    }
+
+    public static RealmResults<Doctor> getTopDoctorList(String spec, int limit) {
+        return realm.where(Doctor.class)
+                .sort("rating", Sort.DESCENDING)
+                .limit(limit)
+                .findAll();
+    }
+
+    public static RealmResults<Prescription> getPrescriptionList() {
+        return realm.where(Prescription.class)
+                .sort("time", Sort.DESCENDING)
+                .findAll();
+    }
+
+    public static RealmResults<Message> getLatestMessageList() {
+        return realm.where(Message.class)
+                .sort("time", Sort.DESCENDING)
+                .distinct("conservation")
+                .findAll();
+    }
+
+    public static RealmResults<Message> getMessageList(String conservation) {
+        return realm.where(Message.class)
+                .equalTo("conservation", conservation)
+                .sort("time", Sort.DESCENDING)
+                .findAll();
+    }
+
+    public static RealmResults<Schedule> getScheduleList() {
+        return realm.where(Schedule.class)
+                .sort("time", Sort.DESCENDING)
+                .findAll();
     }
 
     public static Patient getPatient() {
         return realm.where(Patient.class).findFirst();
+    }
+
+    public static Patient getPatient(String _id) {
+        return realm.where(Patient.class).equalTo("_id", _id).findFirst();
     }
 
     public static Doctor getDoctor() {
@@ -168,5 +259,9 @@ public final class SenoDB {
 
     public static Doctor getDoctor(String _id) {
         return realm.where(Doctor.class).equalTo("_id", _id).findFirst();
+    }
+
+    public static Prescription getPrescription(ObjectId _id) {
+        return realm.where(Prescription.class).equalTo("_id", _id).findFirst();
     }
 }
