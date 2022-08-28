af297c40742a266509d2787f4678a0fd308ba8fe
khoa <dknguyen176@gmail.com>
2022-08-28 21:56:16 +0700

    add chat patient + doctor

--- app/src/main/java/com/example/senocare/fragments/doctor/DoctorChatFragment.java
+++ app/src/main/java/com/example/senocare/fragments/doctor/DoctorChatFragment.java
@@ -8,10 +8,17 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.senocare.R;
+import com.example.senocare.adapters.ChatListAdapter;
+import com.example.senocare.helper.SenoDB;
 
 public class DoctorChatFragment extends Fragment {
+
+    RecyclerView chatListRecyclerView;
+    ChatListAdapter chatListAdapter;
 
     @Nullable
     @Override
@@ -22,6 +29,17 @@ public class DoctorChatFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        createChatRecyclerView(view);
+    }
 
+    private void createChatRecyclerView(View view) {
+        chatListRecyclerView = view.findViewById(R.id.rec_chat_list);
+        chatListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        chatListAdapter = new ChatListAdapter(
+                getContext(),
+                SenoDB.getLatestMessageList(),
+                R.layout.item_chat_list
+        );
+        chatListRecyclerView.setAdapter(chatListAdapter);
     }
 }
--- app/src/main/java/com/example/senocare/fragments/patient/PatientChatFragment.java
+++ app/src/main/java/com/example/senocare/fragments/patient/PatientChatFragment.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.senocare.R;
+import com.example.senocare.activity.ChatListActivity;
+import com.example.senocare.adapters.ChatListAdapter;
+import com.example.senocare.helper.SenoDB;
 
 public class PatientChatFragment extends Fragment {
+
+    RecyclerView chatListRecyclerView;
+    ChatListAdapter chatListAdapter;
 
     @Nullable
     @Override
@@ -23,6 +31,17 @@ public class PatientChatFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        createChatRecyclerView(view);
+    }
 
+    private void createChatRecyclerView(View view) {
+        chatListRecyclerView = view.findViewById(R.id.rec_chat_list);
+        chatListRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+        chatListAdapter = new ChatListAdapter(
+                getContext(),
+                SenoDB.getLatestMessageList(),
+                R.layout.item_chat_list
+        );
+        chatListRecyclerView.setAdapter(chatListAdapter);
     }
 }
