f1dd7a04cc1d10e5657f4b6c8d90683e03241a8c
khoa <dknguyen176@gmail.com>
2022-08-30 18:58:26 +0700

    add profile in chat

--- app/src/main/java/com/example/senocare/activity/ChatBoxActivity.java
+++ app/src/main/java/com/example/senocare/activity/ChatBoxActivity.java
@@ -1,9 +1,13 @@
 package com.example.senocare.activity;
 
+import static com.example.senocare.helper.SenoDB.IS_PATIENT;
+import static com.example.senocare.helper.SenoDB.getDoctorByEmail;
+import static com.example.senocare.helper.SenoDB.getPatientByEmail;
 import static com.example.senocare.helper.SenoDB.modifyMessage;
 import static com.example.senocare.helper.SenoDB.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,22 +16,28 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.senocare.R;
 import com.example.senocare.adapters.ChatBoxAdapter;
 import com.example.senocare.adapters.ChatListAdapter;
 import com.example.senocare.helper.SenoDB;
+import com.example.senocare.model.Doctor;
 import com.example.senocare.model.Message;
+import com.example.senocare.model.Patient;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ChatBoxActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     EditText textMessage;
-    ImageView btnSend;
+    TextView receiver_name;
+    ImageView btnSend, profileBtn;
 
     String sender, receiver, conservation;
+    String receiver_id, name;
 
     RecyclerView chatBoxRecyclerView;
     LinearLayoutManager chatBoxManager;
@@ -45,6 +55,8 @@ public class ChatBoxActivity extends AppCompatActivity {
         createBtnSend();
 
         createChatRecyclerView();
+
+        createToolbar();
     }
 
     @Override
@@ -66,6 +78,16 @@ public class ChatBoxActivity extends AppCompatActivity {
         if (!sender.equals(user.getProfile().getEmail())) {
             receiver = sender;
             sender = user.getProfile().getEmail();
+        }
+
+        if (IS_PATIENT) {
+            Doctor doctor = getDoctorByEmail(receiver);
+            receiver_id = doctor.get_id();
+            name = doctor.getName();
+        } else {
+            Patient patient = getPatientByEmail(receiver);
+            receiver_id = patient.get_id();
+            name = patient.getName();
         }
     }
 
@@ -110,5 +132,37 @@ public class ChatBoxActivity extends AppCompatActivity {
 
     private void createTypeMessage() {
         textMessage = findViewById(R.id.text_message);
+    }
+
+    private void createToolbar() {
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        receiver_name = findViewById(R.id.receiver_name);
+        receiver_name.setText(name);
+
+        profileBtn = findViewById(R.id.profile_btn);
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent;
+                if (IS_PATIENT)
+                    intent = new Intent(ChatBoxActivity.this, DetailedDoctor.class);
+                else
+                    intent = new Intent(ChatBoxActivity.this, DetailedPatient.class);
+                intent.putExtra("_id", receiver_id);
+                intent.putExtra("chat_status", "opened");
+                startActivity(intent);
+            }
+        });
     }
 }
