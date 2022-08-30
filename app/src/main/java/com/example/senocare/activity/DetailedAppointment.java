452ff19b0e285f7954a51d52c349acf0aebd5c97
LeMinhHoangNaruTobaco <111637981+LMHoang20@users.noreply.github.com>
2022-08-30 17:20:17 +0700

    detailed appointment

---
+++ app/src/main/java/com/example/senocare/activity/DetailedAppointment.java
@@ -0,0 +1,16 @@
+package com.example.senocare.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.senocare.R;
+
+public class DetailedAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_appointment);
+    }
+}
